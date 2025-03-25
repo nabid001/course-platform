fc930854d365b190e0eba1f118e707043f1bfa2f
Nabid <nabid.explore@gmail.com>
2025-03-25 20:54:27 +0600

    change webhook env name

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,7 +4,7 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/drizzle/actions/user";
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+  const SIGNING_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!SIGNING_SECRET) {
     throw new Error(
